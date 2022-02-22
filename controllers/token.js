28b1b96dcc5f978eab88c2c6b2e7f22f16298385
ronaldoisc <tellezleal9928@gmail.com>
2022-02-21 21:05:46 -0600

    fix error

--- controllers/token.js
+++ controllers/token.js
@@ -18,7 +18,7 @@ const renewToken =  async (req, res) => {
             let token = body.access_token;
             var today = new Date();
             var currenTime = today.getHours() + "." + today.getMinutes();
-            // let lastDoc = (await Token.find({}).sort({_id: -1}).limit(1))[0];
+            let lastDoc = (await Token.find({}).sort({_id: -1}).limit(1))[0];
 
 
             const tokenSpotify= new Token({token,update_at:currenTime});
